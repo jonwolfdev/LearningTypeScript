c9f72ee9ad36018984e02ecf0fefe3253695f052
Jon <49768794+jonwolfdev@users.noreply.github.com>
2020-06-02 09:19:18 -0500

    Update bootstrap.ts

--- webserver/bootstrap.ts
+++ webserver/bootstrap.ts
@@ -6,3 +6,15 @@ const defController:DefaultController = new DefaultController();
 
 server.addController(defController);
 server.start();
+
+process.on('exit', (code) => {
+    // server.stop();
+});
+
+process.on('SIGINT', (code) => {
+    console.log('');
+    console.log('Got kill signal...');
+    server.stop();
+    console.log('Server is stopped');
+    process.exit(0);
+});
